2b944ce902a1908c0f65ed25de61ad12fc3affbf
AbdallahMooharam <abdomoharamali@gmail.com>
2024-09-04 04:29:52 +0300

    Updates

---
+++ precache-manifest.224b81bd5757506517c7189e5042f183.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9d474927cc8ebdcad3bdf4e4c78fdb",
+    "url": "/Portfolio1/index.html"
+  },
+  {
+    "revision": "23d962365a63b10760f8",
+    "url": "/Portfolio1/static/css/main.53c14c05.chunk.css"
+  },
+  {
+    "revision": "22690811ede213b6687b",
+    "url": "/Portfolio1/static/js/2.5f18e2ad.chunk.js"
+  },
+  {
+    "revision": "23d962365a63b10760f8",
+    "url": "/Portfolio1/static/js/main.b2fee4d6.chunk.js"
+  },
+  {
+    "revision": "f7c9c2ce0e4c0087bf87",
+    "url": "/Portfolio1/static/js/runtime~main.dab7d901.js"
+  },
+  {
+    "revision": "22c839a40b581bcf572b773dddf9a16e",
+    "url": "/Portfolio1/static/media/Education.22c839a4.png"
+  },
+  {
+    "revision": "b5892044e79c273bea7f9fe8e0a75884",
+    "url": "/Portfolio1/static/media/Games.b5892044.png"
+  },
+  {
+    "revision": "c99e8f4036370e3f8e1f63516e395777",
+    "url": "/Portfolio1/static/media/Screenshot_1.c99e8f40.png"
+  },
+  {
+    "revision": "57c1cbdf588176e9f296d3488c3259f9",
+    "url": "/Portfolio1/static/media/Screenshot_12.57c1cbdf.png"
+  },
+  {
+    "revision": "34df5a86f9407c3e15a932af882d669e",
+    "url": "/Portfolio1/static/media/Screenshot_13.34df5a86.png"
+  },
+  {
+    "revision": "72175e3e85c997b2c5f2ca04d8598c23",
+    "url": "/Portfolio1/static/media/Screenshot_14 png.72175e3e.png"
+  },
+  {
+    "revision": "44ee6fed56d3c4c59d0d00da77c6aea3",
+    "url": "/Portfolio1/static/media/Screenshot_2.44ee6fed.png"
+  },
+  {
+    "revision": "f5f6bcc48b3ddf37c2263f1420ae5e9a",
+    "url": "/Portfolio1/static/media/Screenshot_3.f5f6bcc4.png"
+  },
+  {
+    "revision": "d25fb07cbf2c4b244457a8aa5a2eaa48",
+    "url": "/Portfolio1/static/media/Screenshot_4.d25fb07c.png"
+  },
+  {
+    "revision": "2ee0d13dbee7d1c756c39527bc5daa87",
+    "url": "/Portfolio1/static/media/Screenshot_5.2ee0d13d.png"
+  },
+  {
+    "revision": "5491644d60751c046691c3aa511d9fb7",
+    "url": "/Portfolio1/static/media/Screenshot_6.5491644d.png"
+  },
+  {
+    "revision": "bc49fb7f45af41b789a7740a046f399c",
+    "url": "/Portfolio1/static/media/Task.bc49fb7f.jpg"
+  },
+  {
+    "revision": "17397f30b4849b6d6165552ef4688d52",
+    "url": "/Portfolio1/static/media/Updaet CV Abdallah Moharam-8.pdf 3.17397f30.pdf"
+  },
+  {
+    "revision": "322b0b1a02f085fa4b1168a0abfd69c5",
+    "url": "/Portfolio1/static/media/Wooster.322b0b1a.png"
+  },
+  {
+    "revision": "7e6550b5f244290939ab32454e3dfb16",
+    "url": "/Portfolio1/static/media/portfolio3.7e6550b5.jpg"
+  }
+]);
